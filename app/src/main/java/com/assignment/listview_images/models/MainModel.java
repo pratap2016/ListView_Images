f477a4b24fa9442d0d60a6a9de973e9e64504f85
Vijay Pratap <vijay.prasad@mobiliya.com>
2018-08-06 15:51:17 +0530

    Changes model classes for parsing response from server using GSON parsing.

---
+++ app/src/main/java/com/assignment/listview_images/models/MainModel.java
@@ -0,0 +1,36 @@
+package com.assignment.listview_images.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by vijay on 06-08-2018.
+ */
+
+public class MainModel {
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("rows")
+    @Expose
+    private List<RowModel> rows = null;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<RowModel> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<RowModel> rows) {
+        this.rows = rows;
+    }
+
+}
---
+++ app/src/main/java/com/assignment/listview_images/models/RowModel.java
@@ -0,0 +1,45 @@
+package com.assignment.listview_images.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by vijay on 06-08-2018.
+ */
+
+public class RowModel {
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("imageHref")
+    @Expose
+    private Object imageHref;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Object getImageHref() {
+        return imageHref;
+    }
+
+    public void setImageHref(Object imageHref) {
+        this.imageHref = imageHref;
+    }
+
+}
