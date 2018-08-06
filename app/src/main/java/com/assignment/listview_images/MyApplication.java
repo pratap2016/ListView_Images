3a9ae8c2bd84b62b043de14321270f2178f14ea6
Vijay Pratap <vijay.prasad@mobiliya.com>
2018-08-06 16:39:39 +0530

    Changes are: 1. Initialized AppUtil class with Application context. 2. Retrofit instance created with interceptor.

--- app/src/main/java/com/assignment/listview_images/MyApplication.java
+++ app/src/main/java/com/assignment/listview_images/MyApplication.java
@@ -4,6 +4,14 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.assignment.listview_images.utils.AppUtil;
+import com.assignment.listview_images.utils.Constants;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 /**
  * Custom Application class to maintain singleton instance
@@ -11,6 +19,7 @@ import android.support.multidex.MultiDex;
 public class MyApplication extends Application {
 
     private static MyApplication mInstance;
+    private static Retrofit retrofit = null;
 
     /**
      * Creating instance of the class
@@ -31,6 +40,31 @@ public class MyApplication extends Application {
         super.onCreate();
 
         mInstance = this;
+        initClasses();
+    }
+
+    private void initClasses() {
+        new AppUtil(mInstance);
+
+    }
+
+    /**
+     * Retrofit client creation
+     */
+    public static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.Url.BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
     }
 
 
