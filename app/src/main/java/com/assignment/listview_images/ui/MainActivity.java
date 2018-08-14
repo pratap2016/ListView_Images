5a9ccb78068cd426f10dc1669c9d4efb4729acba
Vijay Pratap <vijay.prasad@mobiliya.com>
2018-08-14 20:04:29 +0530

    Changes are: 1. Moved API call and functions to Fragment.

--- app/src/main/java/com/assignment/listview_images/ui/MainActivity.java
+++ app/src/main/java/com/assignment/listview_images/ui/MainActivity.java
@@ -1,45 +1,18 @@
 package com.assignment.listview_images.ui;
 
-import android.Manifest;
-import android.app.ProgressDialog;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.assignment.listview_images.MyApplication;
 import com.assignment.listview_images.R;
-import com.assignment.listview_images.models.MainModel;
-import com.assignment.listview_images.models.RowModel;
-import com.assignment.listview_images.presenter.APICallBacks;
-import com.assignment.listview_images.utils.AppUtil;
-import com.assignment.listview_images.utils.Constants;
 
-import java.util.ArrayList;
-
-import retrofit2.Call;
-import retrofit2.Response;
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageAdapter imageAdapter = null;
-    RecyclerView recyclerView = null;
-    Toolbar toolbar = null;
-    AppCompatTextView tv_Heading = null;
-    TextView tv_Refresh = null;
-    /**
-     * Phone state permission
-     */
-    public static String PERMISSIONS[] = new String[]{Manifest.permission.READ_PHONE_STATE};
+    @BindView(R.id.toolbar_main) Toolbar toolbar;
+    @BindView(R.id.tv_tool_bar_title) AppCompatTextView tv_Heading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,140 +28,53 @@ public class MainActivity extends AppCompatActivity {
      */
     private void ui() {
         initViews();
-        setAdapterToView();
-        loadDataFromServer();
-        viewOnClickListener();
     }
 
-    /**
-     * UI Click listeners
-     */
-    private void viewOnClickListener() {
-        tv_Refresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadDataFromServer();
-            }
-        });
-    }
 
     /**
      * Initializing all views
      */
     private void initViews() {
-        recyclerView = findViewById(R.id.recycler_view_main);
-        toolbar = findViewById(R.id.toolbar_main);
-        tv_Heading = findViewById(R.id.tv_tool_bar_title);
-        tv_Refresh = findViewById(R.id.tv_refresh);
-
         // Setting Toolbar to action bar
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-    }
-
-
-    /**
-     * Setting values to Adapter
-     */
-    private void setAdapterToView(){
-
-        // Creating new instance of Image adapter and setting values to it
-        imageAdapter = new ImageAdapter(new ArrayList<RowModel>(0), new ImageAdapter.PostItemListener() {
-
-            @Override
-            public void onPostClick(String str) {
-                if (null != str)
-                    tv_Heading.setText(str);
-            }
-        });
-
-        // Recycler view Layout manager and Decorator
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(imageAdapter);
-        recyclerView.setHasFixedSize(true);
-        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(itemDecoration);
+        addOrReplaceFragment(R.id.fragment_main, new MainFragment(), MainFragment.class.getName());
     }
 
     /**
-     * Requesting for runtime permission if SDK >= Marshmallow
+     *  Adding or replacing fragment for UI related operations
+     * @param fragment_main
+     * @param mainFragment
+     * @param name
      */
-    private void getPermissions(){
+    private void addOrReplaceFragment(int fragment_main, MainFragment mainFragment, String name) {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (AppUtil.hasSelfPermission(this, PERMISSIONS)) {
-                loadDataFromServer();
-            } else {
-                requestPermissions(PERMISSIONS, Constants.PermissionConatants.REQUEST_CODE);
-            }
+        if(getFragmentManager().getBackStackEntryCount() == 0){
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(fragment_main, mainFragment, name)
+                    .disallowAddToBackStack()
+                    .commit();
+        }else{
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(fragment_main, mainFragment, name)
+                    .disallowAddToBackStack()
+                    .commit();
         }
-        else{
-            loadDataFromServer();
-        }
+
     }
 
-    /**
-     * Method to check internet and Make api call to get feeds
-     */
-    private void loadDataFromServer() {
 
-        if(AppUtil.isInternetConnected(this)) { // Checking internet connection
 
-            // Creating progress dialog to show for async call
-            final ProgressDialog dialog = new ProgressDialog(this);
-            dialog.setCancelable(false);
-            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-            dialog.setMessage(getResources().getString(R.string.loading_wait));
-            dialog.show();
 
-            // Wrapper class callback to get result
-            APICallBacks.getInstance().apiCallToGetData(new APICallBacks.GetResult() {
-                @Override
-                public void onResponse(Call<MainModel> call, Response<MainModel> response) {
-                    dialog.dismiss();
-
-                    // Success response
-                    if (response.isSuccessful()) {
-
-                        // If Response is not empty or null
-                        if(null != response.body()) {
-                            if (null != response.body().getTitle())
-                                tv_Heading.setText(response.body().getTitle());
-                            if (response.body().getRows().size() > 0)
-                                imageAdapter.updateAnswers(response.body().getRows());
-                        }
-                        else{
-                            showErrorMessage();
-                        }
-
-                        Log.d("MainActivity", "posts loaded from API");
-                    } else {
-                        int statusCode = response.code();
-                        showErrorMessage();
-                        // handle request errors depending on status code
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<MainModel> call, Throwable t) {
-                    showErrorMessage();
-                    dialog.dismiss();
-                    Log.d("MainActivity", "error loading from API");
-                }
-            });
+    @Override
+    public void onBackPressed() {
+        if(getFragmentManager().getBackStackEntryCount() >0){
+            getFragmentManager().popBackStack();
         }
-        else{
-            Toast.makeText(MyApplication.getInstance(),getResources().getString(R.string.check_internet), Toast.LENGTH_SHORT).show();
+        else {
+            super.onBackPressed();
         }
-    }
-
-    /**
-     * Show error screen if API call fails
-     */
-    private void showErrorMessage(){
-        findViewById(R.id.ll_main).setVisibility(View.GONE);
-        findViewById(R.id.ll_error).setVisibility(View.VISIBLE);
     }
 }
